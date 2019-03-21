87351fc633155560b2b8c7c3b53e8ca08dca4f1a
GabeLeeData <gabe95sk@gmail.com>
2019-03-20 21:19:04 -0400

    Creating Toast to display at the top instead of bottom

--- app/src/main/java/gabriellee/project/geoquiz/QuizActivity.java
+++ app/src/main/java/gabriellee/project/geoquiz/QuizActivity.java
@@ -2,12 +2,41 @@ package gabriellee.project.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueBotton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueBotton = findViewById(R.id.true_button);
+        mTrueBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
